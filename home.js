407c24464ae3db9fdf175c95d5deb888b68c5952
Mohamedelaribi <mohamed.elaribi.solicode@gmail.com>
2022-09-03 17:27:15 +0100

    up

--- home.js
+++ home.js
@@ -1,1 +1,37 @@
-console.log("hello")
+let categoyList = document.querySelector('.category');
+
+let resutl
+
+
+eventLiseteners();
+
+function eventLiseteners(){
+    window.addEventListener('DOMContentLoaded' ,() =>{
+        loadFileJson();
+    })
+}
+function loadFileJson(){
+    fetch('dogs.json')
+    .then(response => response.json())
+    .then(data =>{
+        let html = '';
+        data.forEach(dogs => {
+            html +=`
+            <div class="card">
+            <div class="imgCard">
+                <div class="frontCard">
+                    
+                        <img src="${dogs.serviceImgSrc}" alt="">
+                    <h2>${dogs.serviceName}</h2>
+                </div>
+                <div class="backCard">
+                    <h2>${dogs.serviceName}</h2>
+                    <p>${dogs.description}</p>
+                    <a href="dogs.html">show more dogs</a>
+                </div>
+            </div>
+        </div>`;
+        });
+        categoyList.innerHTML = html;
+    })
+    }
